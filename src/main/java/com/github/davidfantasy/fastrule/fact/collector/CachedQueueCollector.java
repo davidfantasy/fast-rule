83890f0869de4701c8504d9f8798e6700a9d4d75
wany <davidfantasy8@163.com>
2024-07-21 17:52:07 +0800

    refactor: CachedQueueCollector避免空轮询

--- src/main/java/com/github/davidfantasy/fastrule/fact/collector/CachedQueueCollector.java
+++ src/main/java/com/github/davidfantasy/fastrule/fact/collector/CachedQueueCollector.java
@@ -2,6 +2,7 @@ package com.github.davidfantasy.fastrule.fact.collector;
 
 import com.github.davidfantasy.fastrule.fact.Fact;
 import com.github.davidfantasy.fastrule.fact.FactCollector;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,26 +11,50 @@ import java.util.concurrent.LinkedBlockingQueue;
 /**
  * 自带缓存队列的fact采集器，支持并发采集
  */
-public abstract class CachedQueueCollector implements FactCollector {
+@Slf4j
+public class CachedQueueCollector implements FactCollector {
 
     private final LinkedBlockingQueue<Fact> cacheQueue;
+
+    private final List<Fact> results;
 
     public CachedQueueCollector(int capacity) {
         if (capacity <= 0) {
             throw new IllegalArgumentException("capacity must be greater than 0");
         }
         cacheQueue = new LinkedBlockingQueue<>(capacity);
+        results = new ArrayList<>();
     }
 
     @Override
     public List<Fact> collect() {
-        List<Fact> results = new ArrayList<>();
-        cacheQueue.drainTo(results);
+        results.clear();
+        try {
+            //如果当前没有数据则阻塞collect，直到有数据产生,避免空轮询
+            results.add(cacheQueue.take());
+            //将剩下的数据放入结果集
+            cacheQueue.drainTo(results);
+        } catch (InterruptedException e) {
+            log.error("Thread was interrupted while waiting for facts", e);
+        }
         return results;
     }
 
+    @Override
+    public void startup() {
+
+    }
+
+    @Override
+    public void shutdown() {
+        cacheQueue.clear();
+        results.clear();
+    }
+
     protected void addFact(Fact fact) {
-        cacheQueue.offer(fact);
+        if (!cacheQueue.offer(fact)) {
+            log.warn("cache queue is full,fact will be dropped:{}", fact.getId());
+        }
     }
 
 }
